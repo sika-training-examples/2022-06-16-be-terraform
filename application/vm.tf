cb469a9b116fa5398735be13558903fac7a47ba8
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-16 16:17:05 +0200

    feat(application/vm): Add Worker VM

--- application/vm.tf
+++ application/vm.tf
@@ -9,3 +9,12 @@ module "vm--web" {
   security_groups = local.DEFAULT_SECURITY_GROUPS
   subnet_id       = module.net--default.subnet_ids[1]
 }
+
+module "vm--worker" {
+  source          = "../modules/vm"
+  security_groups = local.DEFAULT_SECURITY_GROUPS
+  subnet_id       = module.net--default.subnet_ids[1]
+  depends_on = [
+    module.vm--db,
+  ]
+}
