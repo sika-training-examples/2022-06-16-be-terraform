e4d1092aaff2e35fa43c9dd2e72d764b08df8e55
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-16 15:04:16 +0200

    feat(ec2): Create some ec2 instances using VM module

--- ec2_example.tf
+++ ec2_example.tf
@@ -1,13 +1,45 @@
-resource "aws_instance" "example" {
-  # ami           = data.aws_ami.debian11.id
-  ami           = local.DEBIAN11
-  instance_type = "t3.micro"
-  key_name      = aws_key_pair.default.key_name
-  security_groups = [
-    aws_security_group.allow-all.name,
-  ]
+module "vm--example" {
+  source          = "./modules/vm"
+  security_groups = local.DEFAULT_SECURITY_GROUPS
 }
 
-output "ip" {
-  value = aws_instance.example.public_ip
+module "vm--foo" {
+  source          = "./modules/vm"
+  security_groups = local.DEFAULT_SECURITY_GROUPS
+  subnet_id       = module.net--foo.subnet_ids[0]
+}
+
+module "vm--bar" {
+  source          = "./modules/vm"
+  security_groups = local.DEFAULT_SECURITY_GROUPS
+  subnet_id       = module.net--bar.subnet_ids[0]
+}
+
+module "vm--baz" {
+  source          = "./modules/vm"
+  security_groups = local.DEFAULT_SECURITY_GROUPS
+  password        = "asdfasdf2022"
+  key_name        = aws_key_pair.default.key_name
+}
+
+output "ec2" {
+  sensitive = true
+  value = {
+    example = {
+      ip       = module.vm--example.public_ip
+      password = module.vm--example.admin-password
+    }
+    foo = {
+      ip       = module.vm--foo.public_ip
+      password = module.vm--foo.admin-password
+    }
+    bar = {
+      ip       = module.vm--bar.public_ip
+      password = module.vm--bar.admin-password
+    }
+    baz = {
+      ip       = module.vm--baz.public_ip
+      password = module.vm--baz.admin-password
+    }
+  }
 }
