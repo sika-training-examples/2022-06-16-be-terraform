f729b65bb0c5d7d96a455d6a10f93d381c006564
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-16 11:38:40 +0200

    feat: Add security group allow all & ec2 instance example

--- main.tf
+++ main.tf
@@ -34,3 +34,36 @@ data "aws_ami" "debian11" {
 output "debian11-ami" {
   value = data.aws_ami.debian11.id
 }
+
+resource "aws_security_group" "allow-all" {
+  name = "allow-all-ondrejsika"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.debian11.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.default.key_name
+  security_groups = [
+    aws_security_group.allow-all.name,
+  ]
+}
+
+output "ip" {
+  value = aws_instance.example.public_ip
+}
